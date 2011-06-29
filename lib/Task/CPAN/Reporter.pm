7ea7b16211dee4e9f50d4e114f8664f0fb3215c5
David Golden <dagolden@cpan.org>
2011-06-29 15:09:05 -0400

    initial stub

---
+++ lib/Task/CPAN/Reporter.pm
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+package Task::CPAN::Reporter;
+# ABSTRACT: No abstract given for Task::CPAN::Reporter
+
+=pod
+
+=pkgroup Modules That are Useful
+
+=pkg Package::Name  0  need at least version 0
+
+=head1 USAGE
+
+This module has no direct functionality.  It is merely a placeholder
+for prerequisites.  Installing it will ensure all the prerequisites listed
+above are satisfied.
+
+=cut
+
+1;
+
